f18fe73fd5a1aab8f7bff732dce386085230897a
jorge.calvache <jorge.calvache@rappi.com>
2025-03-25 13:06:26 -0500

    testing connections

---
+++ app/testing_connection.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+try:
+    engine = create_engine(DATABASE_URL)
+    with engine.connect() as conn:
+        print("✅ Connection successful!")
+except Exception as e:
+    print(f"❌ Connection failed: {str(e)}")
