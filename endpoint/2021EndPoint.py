fc1706cbb398acbace98e463a56712daf87ff0d2
jorge.calvache <jorge.calvache@rappi.com>
2025-03-25 17:19:22 -0500

    add os path to root

--- endpoint/2021EndPoint.py
+++ endpoint/2021EndPoint.py
@@ -7,3 +7,8 @@ from sqlalchemy.orm import sessionmaker
 # Configure database connection
 app = Flask(__name__)
 DATABASE_URL = os.getenv("DATABASE_URL")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# DB connection
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
