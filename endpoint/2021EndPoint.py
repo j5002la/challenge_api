b066ac85cc594db2f363edc23e2895bc2d1a76b8
jorge.calvache <jorge.calvache@rappi.com>
2025-03-25 17:18:36 -0500

    connections done to db

--- endpoint/2021EndPoint.py
+++ endpoint/2021EndPoint.py
@@ -3,5 +3,7 @@ from flask import Flask, jsonify
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
+
+# Configure database connection
 app = Flask(__name__)
-DATABASE_URI = 'postgresql://user:password@localhost:5432/globant_challenge'  # Update credentials
+DATABASE_URL = os.getenv("DATABASE_URL")
